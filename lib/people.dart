27f95393d5d7e87916ad3aea3093696b92ec4964
thomas horga <thomas.horga@gmail.com>
2018-12-18 15:39:42 -0500

    first real commit

---
+++ lib/people.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class People extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("People"),
+      ),
+      body: Center(
+        child: Text(" INSERT PEOPLE"),
+      ),
+    );
+  }
+}
---
+++ lib/teams.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Teams extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Teams"),
+      ),
+      body: Center(
+        child: Text(" INSERT Teams"),
+      ),
+    );
+  }
+}
